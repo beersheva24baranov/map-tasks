5208fe6b4769b3a70ff367ec28894894406497c2
AlexanderBaranov <avbc@mail.ru>
2024-09-25 17:19:39 +0300

    CW #31

--- tasks/src/main/java/map/MapTasks.java
+++ tasks/src/main/java/map/MapTasks.java
@@ -1,6 +1,8 @@
 package map;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapTasks {
 public static void displayOccurrences(String[] strings) {
@@ -32,4 +34,35 @@ private static HashMap<String, Long> getMapOccurrences(String[] strings) {
     Arrays.stream(strings).forEach(s -> result.merge(s, 1l, Long::sum));
     return result;
 }
+public static Map<Integer, Integer[]> getGroupingByNumberOfDigits(int[][] array) {
+
+        Map<Integer, List<Integer>> map = streamOfNumbers(array)
+                .collect(Collectors.groupingBy(n -> Integer.toString(n).length()));
+        return map.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(),
+                e -> e.getValue().toArray(Integer[]::new)));
+
+    }
+
+    private static Stream<Integer> streamOfNumbers(int[][] array) {
+        return Arrays.stream(array).flatMapToInt(Arrays::stream).boxed();
+    }
+
+    public static Map<Integer, Long> getDistributionByNumberOfDigits(int[][] array) {
+
+        return streamOfNumbers(array).collect(Collectors.groupingBy(n -> Integer.toString(n).length(), 
+        Collectors.counting()));
+    }
+    public static void displayDigitsDistribution() {
+        //1_000_000 random numbers from 0 to Integer.MAX_VALUE created
+        //Output should contain all digits (0 - 9) with counters of occurrences
+        //sorted by descending order of occurrences
+        //example:
+        //1 -> <counter of occurrences>
+        //2 -> <counter of occurrences>
+        // ..............
+    }
+    public static ParenthesesMaps getParenthesesMaps(Character[][] openCloseParentheses) {
+        //TODO
+        return null;
+    }
 }
---
+++ tasks/src/main/java/map/ParenthesesMaps.java
@@ -0,0 +1,8 @@
+package map;
+import java.util.Map;
+
+public record ParenthesesMaps(Map<Character, Character> openCloseMap,
+ Map<Character, Character> closeOpenMap) {
+  //openCloseMap - key is openning parentheses, value is closing parenthese
+  //closeOpenMAp - key is closing parentheses , value is openning parentheses
+}
